8ca0a2ade633d43f3a33109e4c2e2ed970ab0180
Derk Barten <derkbarten@gmail.com>
2018-04-26 15:04:53 +0200

    Added analysis of feature value distrubition differentiating formal and informal

--- product/analysis.py
+++ product/analysis.py
@@ -3,10 +3,11 @@ import seaborn
 from util import read_geotiff, concat_tiff
 import matplotlib.pyplot as plt
 import argparse
-from groundtruth import create_mask, create_groundtruth, create_dataset
+from groundtruth import create_mask, create_groundtruth, create_dataset, create_dict
 import logging
 import logging.config
 import math
+import matplotlib.ticker as mtick
 
 
 def boxplot(dataset, axis):
@@ -16,6 +17,11 @@ def boxplot(dataset, axis):
 def value_distribution(feature):
     seaborn.kdeplot(np.ravel(feature))
     plt.show()
+
+
+def feature_distribution(dataset, axis):
+    seaborn.kdeplot(dataset['formal'], ax=axis, label="formal")
+    seaborn.kdeplot(dataset['informal'], ax=axis, label="informal")
 
 
 def spatial_distribution(feature):
@@ -61,6 +67,7 @@ if __name__ == "__main__":
 
     height = features.shape[0] / 3
     width = features.shape[0] / height
+    
 
     # Plot the boxplot of each feature
     fig, axes = plt.subplots(int(width), int(height), sharex='col')
@@ -69,6 +76,8 @@ if __name__ == "__main__":
         dataset = create_dataset(feature, groundtruth)
         axes[i].set_title('feature {}'.format(i + 1))
         boxplot(dataset, axes[i])
+        plt.setp(axes[i].get_xticklabels(), visible=False)
+        plt.setp(axes[i].get_yticklabels(), visible=False)
     fig.show()
 
     # Plot the spatial distribution
@@ -77,11 +86,28 @@ if __name__ == "__main__":
     for i, feature in enumerate(features):
         axes[i].imshow(feature, cmap='gray')
         axes[i].set_title('feature {}'.format(i + 1))
+        plt.setp(axes[i].get_xticklabels(), visible=False)
+        plt.setp(axes[i].get_yticklabels(), visible=False)
     fig.show()
 
     # Plot the groundtruth
     fig, ax = plt.subplots()
     ax.imshow(groundtruth, cmap='gray')
+    plt.setp(ax.get_xticklabels(), visible=False)
+    plt.setp(ax.get_yticklabels(), visible=False)
     fig.show()
 
+    # Plot feature distribution
+    fig, axes = plt.subplots(int(width), int(height), sharex='col')
+    axes = np.ravel(axes)
+    for i, feature in enumerate(features):
+        dataset = create_dict(feature, groundtruth)
+        axes[i].set_title('feature {}'.format(i + 1))
+        
+        #axes[i].yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
+        feature_distribution(dataset, axes[i])
+        plt.setp(axes[i].get_xticklabels(), visible=False)
+        plt.setp(axes[i].get_yticklabels(), visible=False)
+
+    
     plt.show()
