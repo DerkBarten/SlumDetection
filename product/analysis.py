9e0ab367a7196103cd380a306a48a566f716af69
Derk Barten <derkbarten@gmail.com>
2018-05-15 17:00:03 +0200

    Analysis script overhaul

--- product/analysis.py
+++ product/analysis.py
@@ -9,10 +9,8 @@ import logging.config
 import math
 import matplotlib.ticker as mtick
 import re
+import os
 
-
-def boxplot(dataset, axis):
-    seaborn.boxplot(x="formality", y="feature", data=dataset, ax=axis)
 
 
 def value_distribution(feature):
@@ -34,14 +32,32 @@ def analyze(feature):
     value_distribution(feature)
     spatial_distribution(feature)
 
-def normalize(image, n):
-    for i in range(len(image)):
-        mi = np.min(image[i])
-        image[i] =- mi
-        ma = np.max(image[i])
-        image[i] = image[i] / ma
-        image[i] *= n
-    return image
+
+def calculate_padding(image, block, scale):
+    padding_x = math.ceil(image.shape[0] / float(block)) -\
+        math.ceil(float(image.shape[0] - (scale - block)) / block)
+    padding_y = math.ceil(image.shape[1] / float(block)) -\
+        math.ceil(float(image.shape[1] - (scale - block)) / block)
+    return (padding_x, padding_y)
+
+
+def reshape_image(groundtruth, image, block, scale):
+    dimensions = image.shape
+    padding = calculate_padding(image, block, scale)
+
+    x_start = int(math.ceil(padding[0] / 2.0))
+    x_end = int(math.floor(padding[0] / 2.0))
+    y_start = int(math.ceil(padding[1] / 2.0))
+    y_end = int(math.floor(padding[1] / 2.0))
+
+    if x_end <= 0 and y_end <= 0:
+        return groundtruth[x_start:, y_start:]
+    if x_end <= 0:
+        return groundtruth[x_start:, y_start:-y_end]
+    if y_end <= 0:
+        return groundtruth[x_start:-x_end, y_start:]
+    return groundtruth[x_start:-x_end, y_start:-y_end]
+
 
 def parse_name(filename):
     scale = int(re.search('(?<=SC)[0-9]*', filename).group(0))
@@ -49,96 +65,72 @@ def parse_name(filename):
     return scale, block
 
 
+def get_featurefile(imagename, block=20, scale=50, bands=[1, 2, 3],
+                    feature='hog'):
+    return "testfeatures/{}__BD{}-{}-{}_BK{}_SC{}_TR{}.vrt".format(
+                os.path.basename(os.path.splitext(imagename)[0]), bands[0],
+                bands[1], bands[2], block, scale, feature)
+
+
+def create_features(image, blocks, scales, bands, features):
+    cmd = 'spfeas -i {} -o testfeatures --block {} --band-positions {} {} {} --scales {} --triggers {}'
+
+    for feature in features:
+        for block in blocks:
+            for scale in scales:
+                spfeas = cmd.format(image, block, bands[0], bands[1], bands[2],
+                                    scale, feature)
+                os.system(spfeas)
+
+
+def analyze_features(imagefile, blocks, scales, bands, features):
+    image = np.array(read_geotiff(imagefile))
+    shapefile = 'data/slums_approved.shp'
+    mask = create_mask(shapefile, imagefile)[1]
+
+    THRESHOLD = 1000000
+    mask[mask > THRESHOLD] = 0
+
+    for feature in features:
+        for block in blocks:
+            for scale in scales:
+                featurefile = get_featurefile(imagefile, block, scale, bands,
+                                              feature)
+                features_ = np.array(read_geotiff(featurefile))
+                groundtruth = create_groundtruth(mask, block_size=block,
+                                     threshold=0)
+                groundtruth = reshape_image(groundtruth, image, block, scale)
+                
+                for i, feature_ in enumerate(features_):
+                    foldername = "analysis/{}_BK{}_SC{}"
+                    featurename = "{}_{}_BK{}_SC{}_F{}.png"
+                    base = os.path.basename(os.path.splitext(imagefile)[0])
+
+                    featurefolder = foldername.format(base, block, scale)
+                    
+                    if not os.path.exists(featurefolder):
+                        os.mkdir(featurefolder)
+
+                    name = featurename.format(base, 'boxplot', block, scale, i)
+                    boxplot(groundtruth, feature_, featurefolder, name)
+   
+
+def analysis(imagefile, blocks, scales, bands, features):
+    create_features(imagefile, blocks, scales, bands, features)
+    analyze_features(imagefile, blocks, scales, bands, features)
+
+
+def boxplot(groundtruth, feature, folder, name):
+    dataset = create_dataset(feature, groundtruth)
+    seaborn.boxplot(x="formality", y="feature", data=dataset)
+    plt.title(name)
+    plt.savefig(os.path.join(folder, name))
+    plt.clf()    
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Analyze LSR features")
     parser.add_argument("imagefile",  nargs="?",
                         help="The imagefile to use")
-    parser.add_argument("shapefile",  nargs="?",
-                        help="The shapefile to use")
-    parser.add_argument("featurefile",  nargs="?",
-                        help="The featurefile to use")
-    parser.add_argument("--block_size",  nargs="?",
-                        type=int, help="The blocksize")
-
-    logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': True,
-    })
-
-    logging.basicConfig(level=logging.INFO)
-
     args = parser.parse_args()
-
-    features = np.array(read_geotiff(args.featurefile))
-    image = np.array(read_geotiff(args.imagefile))
-    mask = create_mask(args.shapefile, args.imagefile)[4]
-
-
-   
-    THRESHOLD = 1000000
-    #mask[mask < THRESHOLD] = 1
-    mask[mask > THRESHOLD] = 0
-
-    groundtruth = create_groundtruth(mask, block_size=args.block_size,
-                                     threshold=0)
-    #groundtruth = groundtruth[2:, 4:]
-
-    logging.info("Number of features:\t{}".format(features.shape[0]))
-    logging.info("Shape of mask:\t{}".format(mask.shape))
-    logging.info("Shape of image:\t{}".format(image.shape))
-    logging.info("Shape of features:\t{}".format(features.shape))
-    logging.info("Shape of groundtruth:\t{}".format(groundtruth.shape))
-
-    height = features.shape[0] / 3
-    width = features.shape[0] / height
-
-    #img = normalize(image, 2047)
-    image[image > THRESHOLD] = 0
-    plt.imshow(image[4])
-    plt.show()
-    #print(features[4])
-    
-    exit()
-
-    # # Plot the boxplot of each feature
-    # fig, axes = plt.subplots(int(width), int(height), sharex='col')
-    # axes = np.ravel(axes)
-    # for i, feature in enumerate(features):
-    #     dataset = create_dataset(feature, groundtruth)
-    #     axes[i].set_title('feature {}'.format(i + 1))
-    #     boxplot(dataset, axes[i])
-    #     plt.setp(axes[i].get_xticklabels(), visible=False)
-    #     plt.setp(axes[i].get_yticklabels(), visible=False)
-    # fig.show()
-
-    # Plot the spatial distribution
-    fig, axes = plt.subplots(int(width), int(height), sharex='col',)
-    axes = np.ravel(axes)
-    for i, feature in enumerate(features):
-        axes[i].imshow(feature, cmap='gray')
-        axes[i].set_title('feature {}'.format(i + 1))
-        plt.setp(axes[i].get_xticklabels(), visible=False)
-        plt.setp(axes[i].get_yticklabels(), visible=False)
-    fig.show()
-
-    # Plot the groundtruth
-    fig, ax = plt.subplots()
-    ax.imshow(groundtruth, cmap='gray')
-    plt.setp(ax.get_xticklabels(), visible=False)
-    plt.setp(ax.get_yticklabels(), visible=False)
-    fig.show()
-
-    # # Plot feature distribution
-    # fig, axes = plt.subplots(int(width), int(height), sharex='col')
-    # axes = np.ravel(axes)
-    # for i, feature in enumerate(features):
-    #     dataset = create_dict(feature, groundtruth)
-    #     axes[i].set_title('feature {}'.format(i + 1))
-        
-    #     #axes[i].yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
-    #     feature_distribution(dataset, axes[i])
-    #     plt.setp(axes[i].get_xticklabels(), visible=False)
-    #     plt.setp(axes[i].get_yticklabels(), visible=False)
-
-    
-    plt.show()
+    analysis(args.imagefile, [20, 40], [50, 100], [1, 2, 3], ['hog', 'lsr'])
